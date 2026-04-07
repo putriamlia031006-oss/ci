e1c2e8574e609feff5ec2a4fcd4dc42c8d7b06d9
putriamlia031006-oss <putri.amlia031006@gmail.com>
2026-04-07 15:23:01 +0700

    pertemuan 7

--- application/views/kategori/tambah.php
+++ application/views/kategori/tambah.php
@@ -2,7 +2,7 @@
 <h2 class="h3 mb-4 text-gray-800">Tambah Kategori</h2>
 
 <div class="card shadow">
-    <div class="card body">
+    <div class="card-body">
 <form method="post" action="<?= site_url('kategori/simpan'); ?>">
     <div class="form-group">
     <label>Nama Kategori</label><br>
