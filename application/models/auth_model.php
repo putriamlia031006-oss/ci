ec3a4632f2b91436bb116592db48bb4c1d41e917
putriamlia031006-oss <putri.amlia031006@gmail.com>
2026-04-28 15:53:57 +0700

    pertemuan 8

---
+++ application/models/auth_model.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class auth_model extends CI_model{
+
+    public function cek_login($username, $password)
+    {
+        return $this->db->get_where('users',[
+            'username'=>$username,
+            'password'=>md5(password)
+        ])->row();
+    }
+
+    public function upadate_last_login($id)
+    {
+        $this->db->where('id', $id);
+        $this->db->update('users',[
+            'last_login'=>date('y-m-d h:i:s')
+        ]);
+    }
+}
